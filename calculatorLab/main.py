5418a57273bb9cb6652df44dd7298f8361eb8dcf
r-parkhi <radhika.pparkhi@gmail.com>
2025-07-16 10:45:41 -0700

    feature: calculator

--- calculatorLab/main.py
+++ calculatorLab/main.py
@@ -1,24 +1,28 @@
 # main.py
-# Todo: Import operations
-from .operations import add, sub, mul, div
+from operations import add, sub, mul, div;
 
 def main():
-  print("Welcome to the Calculator!")
+  print("Welcome to the Calculator!");
 
-  #TODO: get user input (float) for two numbers
-  
-  #TODO: ask user for operation
+  # Ask for numbers & operation
+  a = float(input("Enter the first number: "));
+  b = float(input("Enter the second number: "));
+  op = str(input("Enter the operation (+, -, *, /): "));
 
-  #TODO: use if statements to perform the correct operation
-  # if op == '+':
-    #result = add(a, b)
-  #elif op == '-':
-    #result = sub(a, b)
-  #...
 
-#print the result 
-#print("Result: " + result)
+  # Call functions
+  if op == "+":
+    result = add(a, b);
+  elif op == "-":
+    result = sub(a, b);
+  elif op == "*":
+    result = mul(a, b);
+  elif op == "/":
+    result = div(a, b);
+
+  # Print result
+  print(f"{a} {op} {b} = {result}");
 
 #running program 
 if __name__ == "__main__":
-    main()
+    main();
